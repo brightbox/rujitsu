0e2c69f2cc99df63a1243913f8c3e5f4e3c1ead2
Caius Durling <git@caius.name>
2008-11-24 13:08:14 +0000

    Fleshing out and finishing the Numeric#to_cents specs

--- spec/rujitsu_spec.rb
+++ spec/rujitsu_spec.rb
@@ -3,9 +3,14 @@ require File.join(File.dirname(__FILE__) + "/spec_helper")
 describe Numeric, "to_cents" do
   it "should be a method" do
     Numeric.instance_methods.should include("to_cents")
+    Numeric.new.should respond_to(:to_cents)
   end
 
-  it "should convert float values to \"cents\""
+  it "should convert float values to \"cents\"" do
+    [ 5, 10, 15, 25, 29 ].each do |i|
+      i.to_cents.should == (i*100).to_i
+    end
+  end
 end
 
 describe Fixnum do
