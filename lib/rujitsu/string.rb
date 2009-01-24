0484c5973eeb5f0ebcbadd94304dee32bab0d50f
Caius Durling <dev@caius.name>
2009-01-24 18:58:04 +0000

    Adding String#truncate with full specs

--- lib/rujitsu/string.rb
+++ lib/rujitsu/string.rb
@@ -1,6 +1,26 @@
 class String
 	# Return a string that can be used as part of a url
+	# replaces basic "bad" characters with "-"
 	def to_url
 		self.downcase.gsub(/[^\-0-9a-z ]/, '').split.join('-')
 	end
+	
+	# Truncates a string to the specified length,
+	# and appends suffix if required
+	# 
+	# Options:
+	# * +length+ length to truncate string to. Includes the suffix in the length. Default is 50.
+	# * +suffix+ suffix to append to truncated string. Pass "" or false for no suffix. Default is "...".
+	# 
+	def truncate opts = {}
+	  opts[:length] ||= 50
+	  opts[:suffix] = opts.has_key?(:suffix) ? opts[:suffix] : "..."
+	  opts[:suffix] ||= ""
+    opts[:length] -= (opts[:suffix].length+1)
+    if opts[:length] > 0
+      self.length > opts[:length] ? self[0..opts[:length]] + opts[:suffix] : self
+    else
+      opts[:suffix][0..(opts[:length] += opts[:suffix].length)]
+    end
+  end
 end
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -19,3 +19,81 @@ describe String, "to_url" do
   end
 
 end
+
+describe String, "truncate" do
+  
+  it "should return shorter string unmodified with no params" do
+    str = 30.random_characters
+    # default length is 50
+    str.truncate.should == str
+  end
+  
+  it "should return shorter string unmodified with length param" do
+    str = "0123456789" # length 10
+    trunced = str.truncate(:length => 30)
+    trunced.length.should == 10
+    trunced.should == str
+  end
+  
+  it "should return shorter string unmodified with suffix param" do
+    str = "0123456789" # length 10
+    trunced = str.truncate(:suffix => "abcd")
+    trunced.length.should == 10
+    trunced.should == str
+  end
+  
+  it "should return shorter string unmodified with length and suffix params" do
+    str = "0123456789" # length 10
+    trunced = str.truncate(:length => 30, :suffix => "abcd")
+    trunced.length.should == 10
+    trunced.should == str
+  end
+  
+  it "should truncate longer string with no params" do
+    str = "this is a string with a really long length just to get above 50 characters"
+    # default length is 50
+    # default suffix is "..."
+    trunced = str.truncate
+    trunced.length.should == 50
+    trunced.should == "this is a string with a really long length just..."
+  end
+
+  it "should truncate longer string with length param" do
+    str = "123456789123456789" # length 11
+    trunced = str.truncate(:length => 10)
+    trunced.length.should == 10
+    trunced.should == "1234567..."
+  end
+  
+  it "should truncate longer string with suffix param" do
+    str = "this is a string with a really long length just to get above 50 characters"
+    # default length is 50
+    trunced = str.truncate(:suffix => "..abc")
+    trunced.length.should == 50
+    trunced.should == "this is a string with a really long length ju..abc"
+  end
+  
+  it "should truncate longer string with length and suffix param" do
+    str = "123456789123456789" # length 11
+    trunced = str.truncate(:length => 10, :suffix => "abde")
+    trunced.length.should == 10
+    trunced.should == "123456abde"
+  end
+  
+  it "should return truncated string with no suffix if suffix param is false" do
+    str = "this is a string with a really long length just to get above 50 characters"
+    # default length is 50
+    trunced = str.truncate(:suffix => false)
+    trunced.length.should == 50
+    trunced.should == "this is a string with a really long length just to"
+  end
+  
+  it "should return a string of length specified even when the suffix is longer" do
+    str = "this is a string" # => 16
+    suff = "this is a longer string" # => 23
+    trunced = str.truncate(:suffix => suff, :length => 10)
+    trunced.should == "this is a "
+    trunced.length.should == 10
+  end
+  
+end
