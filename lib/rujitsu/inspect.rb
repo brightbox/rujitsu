1c4b4a045c8b5474a588804ea4ce1c6329250347
Caius Durling <dev@caius.name>
2009-04-08 17:40:18 +0100

    Adding a few debugging methods. require "rujitsu/inspect" to use them.

---
+++ lib/rujitsu/inspect.rb
@@ -0,0 +1,24 @@
+module Kernel
+  # If output is true then echo's,
+  # else just returns the name 
+  def _cmd output=false
+    caller[0] =~ /`([^']*)'/
+    output ? p($1) : $1
+  end
+  private :_cmd
+end
+
+module ExtendObject
+  # Outputs all methods not inherited from Object
+  def simple_methods
+    self.methods.sort - Object.methods
+  end
+  
+  # Matches your search string against all
+  # methods and returns array
+  def grep_method match
+    match = /#{match}/ unless match.is_a? Regexp
+    self.methods.grep(match)
+  end
+end
+Object.send(:include, ExtendObject)
