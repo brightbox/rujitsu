4365c5bbfdda6a4d79a3fe1f68faf3a8d6519e4b
Pavel <worldisaduck@gmail.com>
2017-04-26 21:33:41 +0300

    small spacing fixes

--- lib/rujitsu/date.rb
+++ lib/rujitsu/date.rb
@@ -2,17 +2,17 @@ module RujitsuDate
 
   def self.start_of_this_month
     today = self.today
-    self.new (today.year, today.month)
+    self.new(today.year, today.month)
   end
 
   def self.start_of_last_month
     today = self.today
-    self.new (today.year, today.month) - 1.month
+    self.new(today.year, today.month) - 1.month
   end
 
   def self.start_of_next_month
     today = self.today
-    self.new (today.year, today.month) + 1.month
+    self.new(today.year, today.month) + 1.month
   end
 
 end
