4ea7e9deb3283906c613678b7d96cb72624bada1
Pavel <worldisaduck@gmail.com>
2017-04-26 22:13:50 +0300

    removed unnecessary selfs

--- lib/rujitsu/date.rb
+++ lib/rujitsu/date.rb
@@ -1,16 +1,16 @@
 module RujitsuDate
 
-  def self.start_of_this_month
+  def start_of_this_month
     today = self.today
     self.new(today.year, today.month)
   end
 
-  def self.start_of_last_month
+  def start_of_last_month
     today = self.today
     self.new(today.year, today.month) - 1.month
   end
 
-  def self.start_of_next_month
+  def start_of_next_month
     today = self.today
     self.new(today.year, today.month) + 1.month
   end
